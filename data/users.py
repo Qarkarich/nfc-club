13155e4e0d1ca420fe7a8c5bd96eeb3d0722648d
Fedor Blinov <fedyoiblinov@gmail.com>
2022-04-12 15:48:35 +0300

    basic structure + basic user model

---
+++ data/users.py
@@ -0,0 +1,22 @@
+from .db_sessions import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+import sqlalchemy
+import datetime
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    modified_data = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_login import LoginManager, login_user, logout_user
+from data import db_sessions
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'qwaszxer'
+login_manager = LoginManager()
+
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+
+def main():
+    db_sessions.global_init("db/main")
+    app.run(host='127.0.0.1', port='8080')
+
+
+if __name__ == '__main__':
+    main()
