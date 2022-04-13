ccd10d4786d09975424b3417437413ecd477dfd8
Platon <69628622+MrChester-X@users.noreply.github.com>
2022-04-13 20:22:54 +0300

    Init repository 1.0 v3 (#3)
    Start of repository

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+# from flask_login import LoginManager, login_user, logout_user
+from data import db_session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "qwaszxer"
+# login_manager = LoginManager()
+
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+
+def main():
+    db_session.global_init("db/main.sqlite")
+    app.run(host="127.0.0.1", port=8080)
+
+
+if __name__ == "__main__":
+    main()
