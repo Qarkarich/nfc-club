5186c521dec707ff630ed76aa0c983aaad045ae6
Fedor Blinov <fedyoiblinov@gmail.com>
2022-04-14 21:07:40 +0300

    Authorisation

--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, redirect
-# from flask_login import LoginManager, login_user, logout_user
+from flask_login import LoginManager, login_user, logout_user, current_user
 from data import db_session
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from data.users import User
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "qwaszxer"
-# login_manager = LoginManager()
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route("/")
 def index():
-    return "Hello, world!"
-
-
-def main():
-    db_session.global_init("db/main.sqlite")
-    app.run(host="127.0.0.1", port=8080)
+    if current_user.is_authenticated:
+        return ":)))"
+    else:
+        return ":((("
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -47,6 +51,27 @@ def reqister():
         return redirect("/login")
 
     return render_template("register.html", title="Регистрация", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html",
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template("login.html", title="Авторизация", form=form)
+
+
+def main():
+    db_session.global_init("db/main.sqlite")
+    login_manager.init_app(app)
+    app.run(host="127.0.0.1", port=8080)
 
 
 if __name__ == "__main__":
