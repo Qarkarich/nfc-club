5e3a59c6fa2d325635c4956839b12ae171eac65b
Fedor Blinov <fedyoiblinov@gmail.com>
2022-04-14 20:52:42 +0300

    Add User login form

--- src/forms/user.py
+++ src/forms/user.py
@@ -9,3 +9,10 @@ class RegisterForm(FlaskForm):
     password = PasswordField("Пароль", validators=[DataRequired()])
     password_again = PasswordField("Повторите пароль", validators=[DataRequired()])
     submit = SubmitField("Войти")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
