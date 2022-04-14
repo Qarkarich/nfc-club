c8becacd05a77f7205bec32144ae394ce6da3518
Fedor Blinov <fedyoiblinov@gmail.com>
2022-04-14 20:49:31 +0300

    Update User class

--- src/data/users.py
+++ src/data/users.py
@@ -1,15 +1,16 @@
 from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_login import UserMixin
+from flask_login import UserMixin
 import sqlalchemy as sa
 from sqlalchemy import orm
 import datetime as dt
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = "users"
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    email = sa.Column(sa.String, unique=True)
     name = sa.Column(sa.String, nullable=True)
     hashed_password = sa.Column(sa.String)
     created_data = sa.Column(sa.DateTime, default=dt.datetime.now())
