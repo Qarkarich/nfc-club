dda4b2e258d8aa85fe5c03eb12c123cbd521a330
MrChester-X <bulychev_platon1@mail.ru>
2022-04-17 17:16:54 +0300

    Class `User` update for cards

--- src/data/users.py
+++ src/data/users.py
@@ -1,9 +1,11 @@
-from .db_session import SqlAlchemyBase
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-import sqlalchemy as sa
-from sqlalchemy import orm
 import datetime as dt
+
+import sqlalchemy as sa
+from flask_login import UserMixin
+from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase, UserMixin):
@@ -13,12 +15,15 @@ class User(SqlAlchemyBase, UserMixin):
     email = sa.Column(sa.String, unique=True)
     name = sa.Column(sa.String, nullable=True)
     hashed_password = sa.Column(sa.String)
-    created_data = sa.Column(sa.DateTime, default=dt.datetime.now())
+    created_data = sa.Column(sa.DateTime, default=dt.datetime.now)
 
-    cards = orm.relation("Card", back_populates="users")
+    cards = orm.relation("Card", back_populates="user")
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+    def is_get_card(self, card):
+        return card.id in [i.id for i in self.cards]
